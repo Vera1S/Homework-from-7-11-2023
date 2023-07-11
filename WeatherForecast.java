d04907cb47ad09ab144c77aaededcfd0b4f9f3bf
Vera1S <128926912+Vera1S@users.noreply.github.com>
2023-07-11 16:13:32 -0400

    Add files via upload
    Создать класс с прогнозом погоды в любом пакете.
    
    В классе должна содержаться информация о: днях недели, погоде на каждый день недели, субъективную оценку на день недели "хорошая ли погода".
    
    Создать объект класса прогноза погоды в классе "Main", и вывести всю информацию о прогнозе на каждый день недели.
    
    Конкретное содержание класса с прогнозом остаётся на ваше усмотрение. Главное - чтобы в нём содержалась вся необходимая для выполнения задания информация.

---
+++ WeatherForecast.java
@@ -0,0 +1,28 @@
+package Homework;
+
+public class WeatherForecast {
+
+//    Создать класс с прогнозом погоды в любом пакете.
+//
+//В классе должна содержаться информация о: днях недели,
+// погоде на каждый день недели, субъективную оценку на день недели "хорошая ли погода".
+//
+//Создать объект класса прогноза погоды в классе "Main", и вывести всю информацию о
+// прогнозе на каждый день недели.
+//
+//
+//Конкретное содержание класса с прогнозом остаётся на ваше усмотрение. Главное - чтобы
+// в нём содержалась вся необходимая для выполнения задания информация.
+    public String  monday = "It's raining - bad weather";
+    public String  tuesday = "Windy - not bad weather";
+    public String  wednesday = "Sunny - It's good weather";
+    public String  thursday = "Cloudy - not bad weather";
+    public String  friday = "Sunny - It's good weather";
+    public String  saturday = "Windy - not bad weather";
+    public String  sunday = "Sunny weather - It's good weather";
+
+
+
+
+
+}
